41b757461b71336502942bb48c2ff6e45cee0f79
Burak Ilgin <burakilginnn@gmail.com>
2025-11-15 23:59:14 +0300

    Initial Commit

---
+++ OneButtonToggle/OneButtonToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneButtonToggleApp.swift
+//  OneButtonToggle
+//
+//  Created by Burak Ilgın on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct OneButtonToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
