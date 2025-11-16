533adac7430c27e32e9766c434d5d2166d127d32
Burak Ilgin <burakilginnn@gmail.com>
2025-11-16 11:49:34 +0300

    First Toogle with Prof G.

--- OneButtonToggle/ContentView.swift
+++ OneButtonToggle/ContentView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = ""
+    @State private var imageView = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(systemName: imageView)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.thin)
+            
+            Spacer()
+            
         }
+        
+        Button("Press Me!") {
+            if message == "You are awesome!" {
+                message = "You are Great!"
+                imageView = "hand.thumbsup"
+            } else {
+                message = "You are awesome!"
+                imageView = "sun.max.fill"
+            }
+        }
+        
+            .tint(.orange)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 10))
+
         .padding()
     }
 }
