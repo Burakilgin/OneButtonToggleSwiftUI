309110068b32c8cd108a743bb97d06a9e610648d
Burak Ilgin <burakilginnn@gmail.com>
2025-11-25 16:59:55 +0300

    Loop section added

--- OneButtonToggle/ContentView.swift
+++ OneButtonToggle/ContentView.swift
@@ -13,32 +13,42 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var imageNumber = 0
     @State private var startMessage = ""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     
-    let messageArray = ["image0", "image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9"]
+    
+    let messageArray = ["You Are Awesome", "Fabulous", "Amazing", "Creative", "What do you think about that images?", "image5", "Gadzook my friend! I am astonished at how utterly magnificent you are", "image7", "image8", "image9"]
     let imageArray = ["image0", "image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9"]
+    
     
     var body: some View {
         VStack {
             
             Spacer()
             
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .frame(height: 130)
+                .minimumScaleFactor(0.5)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            
+                .animation(.default, value: imageName)
+//bouncy(duration: 0.5, extraBounce: 0.2)
             
             Spacer()
             
         }
         
-        Button("Show Message Me!") {
+        Button("Show Message") {
             
             /*if message == message1 {
                 message = message2
@@ -48,17 +58,35 @@ struct ContentView: View {
                 imageView = imageString2
             }*/
             
-            imageNumber += 1
+            var messageNumber : Int
+            repeat {
+                messageNumber = Int.random(in: 0...messageArray.count-1)
+                
+            }while messageNumber == lastMessageNumber
+            
+            message = messageArray[messageNumber]
+            lastMessageNumber = messageNumber
+            
+            var imageNumber : Int
+            repeat {
+                imageNumber = Int.random(in: 0...9)
+            } while imageNumber == lastImageNumber
+            imageName = "image\(imageNumber)"
+            lastImageNumber = imageNumber
+            
+            
+            //imageName = imageArray[Int.random(in: 0...imageArray.count-1)]
+            //message = messageArray[Int.random(in: 0...messageArray.count - 1)]
+                       
+            
+/*            imageNumber += 1
             
             
             if imageNumber > 9 {
                 imageNumber = 0
                 
-            }
-            
-            imageName = imageArray[imageNumber]
-            message = messageArray[imageNumber]
-            
+            }*/
+
             
             print("Image Number: \(imageNumber)")
             
